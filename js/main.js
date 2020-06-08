2abf5f2faa44c5234d2d45a01aba320c979bdc65
ampere-dev <54696061+ampere-dev@users.noreply.github.com>
2020-06-08 09:14:28 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    $('.box-carousel').slick({
+     dots: false,
+     arrows: false,
+     infinite: false,
+     centerMode: false,
+     slidesToShow: 20,
+     prevArrow: "<button type='button' class='mission-prev-arrow'><i class='fa fa-3x fa-chevron-circle-left'></i></button>",
+     nextArrow: "<button type='button' class='mission-next-arrow'><i class='fa fa-3x fa-chevron-circle-right'></i></button>"
+ });
+
+});
